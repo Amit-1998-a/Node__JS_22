dd9b7c876182407f31552a145cf9398d8bac04a1
Amit <amitsonavane698@gmail.com>
2022-06-23 14:23:48 +0530

    23-06-22

--- readble.js
+++ readble.js
@@ -3,9 +3,10 @@ var http = require('http')
 var fs= require('fs')
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf-8');
+var myWriteStream= fs .createWriteStream(__dirname+ '/writeMe.txt');
 
 myReadStream.on('data',function(chunk){
-    console.log('new chunk received: ')
-    console.log(chunk);
-})
+    console.log('new chunk received:');
+    myWriteStream.write(chunk);
+});
 
