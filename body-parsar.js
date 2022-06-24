cbdbeb945e241e855df460f2a8b25b1787d63116
Amit <amitsonavane698@gmail.com>
2022-06-24 10:01:23 +0530

    24-06-22

--- body-parsar.js
+++ body-parsar.js
@@ -1,21 +1,21 @@
-var express = require ('express');
-
+var express =  require ('express');
+var bodyParsar = require('body-parsar');
 var app = express()
+var urlencodedParsar = bodyParsar.urlencodedParsar({ extended:false })
 
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('assets')); 
 
-// app.get('/', function(req, res){
-//     res.sendFile(__dirname + '/index.html')
-// });
-// app.get('/contact', function(req, res){
-//     res.sendFile(__dirname + '/contact.html')
-// });
+
 app.get('/', function(req, res){
     res.render('index');
 });
 app.get('/contact', function(req, res){
-    res.render('contact');
+    res.render('contact',{qs:req.query});
+});
+app.post('/contact',urlencodedParsar, function(req, res){
+    console.log(req.body);
+    res.render('contact',{qs:req.query});
 });
 
  app.get('/profile/:name', function(req,res){
