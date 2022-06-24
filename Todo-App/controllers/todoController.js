1a40cf77df0d6e440109677760af5a90e79ae0d1
Amit <amitsonavane698@gmail.com>
2022-06-24 17:29:28 +0530

    24-06-22

--- Todo-App/controllers/todoController.js
+++ Todo-App/controllers/todoController.js
@@ -12,13 +12,16 @@ app.get('/todo',function(req, res){
 
 app.post('/todo',urlencodedParser, function(req, res){
    data.push(req.body);
-   
    res.json(data);
 
 
 });
 
 app.delete('/todo/:item', function(req, res){
+    data = data.filter(function(todo){
+        return todo.item.replace(/ /g, '-') !== req.params.item;
+
+    });
 
 
 });
