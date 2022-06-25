f6ddd817ebeca50d242458e1dfdde61fd7382620
Amit <amitsonavane698@gmail.com>
2022-06-25 18:16:02 +0530

    6.15 25-06-22

---
+++ practise/demo.js
@@ -0,0 +1,60 @@
+//hello console
+
+
+
+console.log("Hello")
+
+
+
+
+//date
+
+let myDate = new Date();
+
+console.log(myDate.getTime());
+
+console.log(myDate.getFullYear());
+
+console.log(myDate.getMonth())
+
+
+
+
+ //Dom Manipulation
+
+
+
+ let elem =document.getElementById("click");
+
+ console.log(elem)
+
+
+
+ let elemClass = document.getElementsByClassName("container")
+
+ console.log(elemClass);
+
+ elemClass[0].style.background="cyan"
+
+
+
+
+ function clicked(){
+
+     console.log("the button was click")
+
+ }
+
+ window.onload=function(){
+
+    console.log('The document was loaded')
+
+ }
+
+ 
+
+
+
+ //Event in java Script//
+
+ 
