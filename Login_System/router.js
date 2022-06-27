c22ec5ca4a430d9880f89b61d202a40c30787f3b
Amit <amitsonavane698@gmail.com>
2022-06-27 16:06:07 +0530

    27-06-22

--- Login_System/router.js
+++ Login_System/router.js
@@ -2,20 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 var credential ={
-    email:'admin@gmail.com',
+    email:"admin@gmail.com",
     password:"admin123"
 }
 
 
 //login user
 router.post('/login',(req,res)=>{
-    if(req.body.email== credential.email && req.body.password== credential.password){
+    if(req.body.email==credential.email && req.body.password== credential.password){
         req.session.user = req.body.email
-       // res.redirect('/dashboard')
-       res.end('Login Sucess')
+        res.redirect('route/dashboard')
+      // res.end('Login Sucess')
     }else{
         res.end('Invalid Username')
     }
 })
+
+// router for dashboard
+
+router.get('/dashboard',(req,res)=>{
+    if(req.session.user){
+        res.render('dashboard',{user:req.session.user})
+    }else{
+        res.send("Unauthorize User")
+    }
+})
+
+
 
 module.exports=router;
