3e2415acd50a5fcbf502426984d3807cfe979e28
Amit <amitsonavane698@gmail.com>
2022-06-27 15:10:36 +0530

    27-06-22

--- Login_System/server.js
+++ Login_System/server.js
@@ -1,9 +1,31 @@
+var bodyParser = require('body-parser');
 var express = require('express')
 var app =express(); 
+var session = require('express-session');
+var {v4:uuidv4}= require('uuid');
+var router = require('./router')
+
+
  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
 
 app.set('view engine','ejs')
+
+// load static assets
+
+app.use(session({
+    secret : uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}));
+
+app.use('/route',router);
+
+app.use('/assets',express.static('assets'))
 
 // home route 
 
